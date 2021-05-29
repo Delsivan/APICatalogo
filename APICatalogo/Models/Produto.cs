1b0c046e6de91f014eb3f46b3c8db07a2985575f
Delsivan Nascimento Vieira <delsivannascimentovieira@gmail.com>
2021-05-29 12:06:32 -0300

    Model validation: IValidatableObject

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -1,18 +1,19 @@
 ﻿using APICatalogo.Validations;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APICatalogo.Models
 {
     [Table("Produtos")]
-    public class Produto
+    public class Produto : IValidatableObject
     {
         [Key]
         public int ProdutoId { get; set; }
         [Required(ErrorMessage ="O nome é obrigatório")]
         [StringLength(20, ErrorMessage ="O nome deve tem entre 2 e 20 caracteres", MinimumLength = 2)]
-        [PrimeiraLetraMaiuscula]
+        //[PrimeiraLetraMaiuscula]
         public string Nome { get; set; }
         [Required]
         [StringLength(50, ErrorMessage = "A descrição deve ter no maximo{1} caracteres")]
@@ -28,5 +29,29 @@ namespace APICatalogo.Models
 
         public Categoria Categoria { get; set; }
         public int CategoriaId { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(this.Nome))
+            {
+                var primeiraLetra = this.Nome[0].ToString();
+                if (primeiraLetra != primeiraLetra.ToUpper())
+                {
+                    yield return new
+                        ValidationResult("A primeira letra do produto deve ser maiúscula",
+                        new[]
+                        { nameof(this.Nome) }
+                        );
+                }
+            }
+            if(this.Estoque <= 0)
+            {
+                yield return new
+                        ValidationResult("O estoque deve ser maior que zero",
+                        new[]
+                        { nameof(this.Estoque) }
+                        );
+            }
+        }
     }
 }
