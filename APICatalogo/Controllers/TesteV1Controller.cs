7ae9ddbaf14ad4ee0c5277c9b934775445dca0cf
Delsivan Nascimento Vieira <delsivannascimentovieira@gmail.com>
2021-06-10 21:06:49 -0300

    Versioning of methods

--- APICatalogo/Controllers/TesteV1Controller.cs
+++ APICatalogo/Controllers/TesteV1Controller.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace APICatalogo.Controllers
 {
-    [ApiVersion("1.0", Deprecated = true)]
+    [ApiVersion("1.0")]
+    [ApiVersion("2.0")]
     [Route("api/v{v:apiVersion}/teste")]
     [ApiController]
     public class TesteV1Controller : Controller
@@ -14,7 +15,12 @@ namespace APICatalogo.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            return Content("<html><body><h2>TesteV1Controller - V 1.0 </h2></body></html>", "text/html");
+            return Content("<html><body><h2>TesteV1Controller - GET V 1.0 </h2></body></html>", "text/html");
+        }
+        [HttpGet, MapToApiVersion("2.0")]
+        public IActionResult GetVersao2()
+        {
+            return Content("<html><body><h2>TesteV1Controller -GET V 2.0 </h2></body></html>", "text/html");
         }
     }
 }
